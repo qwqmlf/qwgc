2098bc60052929dc9634d58bc3da90295580cfca
Chibikuri <ryosuke.satoh.wk@gmail.com>
2020-01-01 20:02:30 +0900

    for pypi

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open('requirements.txt', 'r') as f:
+    requirements = [line.strip() for line in f]
+
+setup(
+    name="qwgc",
+    version="0.0.1",
+    description="Graph classifier based on quantum walk",
+    long_description="qwgc is a quantum walk graph classifier to classify"
+                     "graph data. Works with Python 3.5, 3.6, and 3.7",
+    url="https://Chibikuri.github.io/qwgc",
+    author="Ryosuke Satoh",
+    author_email="ryosuke.satoh.wk@gmail.com",
+    license="Apache 2.0",
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords="quantum walk machine learning",
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires=">=3.5",
+)

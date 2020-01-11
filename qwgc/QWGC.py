d9bca360ef9782963910e14d26a8decac09dca21
Chibikuri <ryosuke.satoh.wk@gmail.com>
2020-01-11 20:21:22 +0900

    ch layer

--- qwgc/QWGC.py
+++ qwgc/QWGC.py
@@ -287,7 +287,7 @@ if __name__ == '__main__':
     k = 5
     kf = KFold(n_splits=k, shuffle=True)
 
-    qwgc = QWGC(['010', '101'], Cp=p_pso['Cp'], Cg=p_pso['Cg'],
+    qwgc = QWGC(['01', '10'], Cp=p_pso['Cp'], Cg=p_pso['Cg'],
                 n_particle=p_pso['particles'], T=p_pso['iterations'],
                 w=p_pso['w'], ro_max=p_pso['random_max'],
                 n_layer=p_pso['layers'], lamb=p_pso['lambda'],

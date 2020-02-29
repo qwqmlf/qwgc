b21b714a79581ea7c1a083cd82f0fe5a053a3aaa
Chibikuri <ryosuke.satoh.wk@gmail.com>
2020-02-29 17:34:22 +0900

    update param

--- qwgc/QWGC.py
+++ qwgc/QWGC.py
@@ -200,7 +200,7 @@ class QWGC:
             accuracy.append(accs)
             if t % 10 == 0 and notify:
                 Notify.notify_error(t, error, accs)
-            if error < 0.40:
+            if error < 0.38:
                 break
             # if t > 10 and np.mean(errors[-10:-1]) < errors[-1]:
             #     # print(particles)
